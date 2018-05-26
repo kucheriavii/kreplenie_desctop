701ae3a3c88ee73a3693434256f2f3e54b9980d7
kucheriavii <kucheriavii@gmail.com>
2018-05-27 01:40:31 +0300

    header for 1440

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
 	// Custom JS
-
+    dropMenuHover();
+    headerButtonActive();
 });
+
+var dropMenuHover = function(){
+    var drop_menu = $('.drop-menu');
+    drop_menu.on('mouseenter', function (){
+        drop_menu.addClass('drop-menu--active');
+    });
+    drop_menu.on('mouseleave', function (){
+        drop_menu.removeClass('drop-menu--active');
+    });
+};
+
+var headerButtonActive = function (e) {
+  var headerButton = $('.header-buttons__item');
+  headerButton.on('click', function(){
+      headerButton.removeClass('active');
+      $(this).toggleClass('active')
+  })
+};
